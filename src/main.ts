e8be960181772dea02a943bdbdca97b3a048f8c9
Gabriel Alejandro Pina <gabrielpinia@hotmail.com>
2025-02-07 22:31:36 -0400

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,37 @@ import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-      .setTitle('Virtual Wallet API')
-      .setDescription('Documentación de los endpoints de la billetera virtual')
-      .setVersion('1.0')
-      .addTag('wallet')
-      .build();
+  const globalPrefix = 'api/db';
+  app.setGlobalPrefix(globalPrefix);
 
-  const document = SwaggerModule.createDocument(app, config);
+  const config = new DocumentBuilder()
+    .setTitle('Virtual Wallet API')
+    .setDescription('Documentación de los endpoints de la billetera virtual')
+    .setVersion('1.0')
+    .addTag('wallet')
+    .build();
+
+  let document = SwaggerModule.createDocument(app, config);
+
+  document = {
+    ...document,
+    paths: Object.keys(document.paths).reduce((acc, currentPath) => {
+      const newPath = currentPath.startsWith(`/${globalPrefix}`)
+        ? currentPath
+        : `/${globalPrefix}${currentPath}`;
+
+      acc[newPath] = document.paths[currentPath];
+      return acc;
+    }, {}),
+  };
 
   SwaggerModule.setup('api-docs', app, document);
 
-  app.setGlobalPrefix('api/db');
-
   await app.listen(4000);
+
   console.log(`Swagger está disponible en: http://localhost:4000/api-docs`);
-  console.log(`El resto de los endpoints están en: http://localhost:4000/api/db`);
+  console.log(`Los endpoints están accediéndose en: http://localhost:4000/${globalPrefix}`);
+
 }
 bootstrap();
 
